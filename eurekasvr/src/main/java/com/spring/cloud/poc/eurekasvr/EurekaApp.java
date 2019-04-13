6eb1489a504e317e8225a3d50438e6d05e746241
nikhil.raj <nikhil.raj@CSN-VM-T3J-0137.corp.trizetto.com>
2019-04-13 15:39:36 +0530

    added eureka server to repository

---
+++ eurekasvr/src/main/java/com/spring/cloud/poc/eurekasvr/EurekaApp.java
@@ -0,0 +1,17 @@
+package com.spring.cloud.poc.eurekasvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApp {
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApp.class, args);
+	}
+}
