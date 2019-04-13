31245606f718412630710de0275f9046852cd31f
nikhil.raj <nikhil.raj@CSN-VM-T3J-0137.corp.trizetto.com>
2019-04-13 15:44:46 +0530

    added organizationservice to repository

---
+++ organizationservice/src/main/java/com/thoughtmechanix/organizationservice/service/OrganizationController.java
@@ -0,0 +1,39 @@
+package com.thoughtmechanix.organizationservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "v1/organizations")
+@RefreshScope
+public class OrganizationController {
+
+	@RequestMapping(method = RequestMethod.GET)
+	public String getOrganization() {
+		return "This is get";
+	}
+
+	@RequestMapping(method = RequestMethod.PUT)
+	public String updateLicenses() {
+		return String.format("This is the put");
+	}
+
+	@RequestMapping(value = "{licenseId}", method = RequestMethod.POST)
+	public String saveLicenses(@PathVariable("licenseId") String licenseId) {
+		return String.format("This is the post");
+	}
+
+	@RequestMapping(value = "{licenseId}", method = RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public String deleteLicenses(@PathVariable("licenseId") String licenseId) {
+		return String.format("This is the Delete");
+	}
+
+}
