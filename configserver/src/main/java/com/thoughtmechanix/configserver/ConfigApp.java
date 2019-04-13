c8fc481008c8f8b52a422d9a70cf2a4d6d5d271f
nikhil.raj <nikhil.raj@CSN-VM-T3J-0137.corp.trizetto.com>
2019-04-13 15:16:27 +0530

    added config server repo

---
+++ configserver/src/main/java/com/thoughtmechanix/configserver/ConfigApp.java
@@ -0,0 +1,17 @@
+package com.thoughtmechanix.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigApp.class, args);
+	}
+}
